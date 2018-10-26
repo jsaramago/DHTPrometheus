fe53aaa3c41759df30771ec4fbd05e7f94e257d6
João Saramago <joao.saramago@sky.uk>
2018-10-26 21:00:02 +0100

    Pushgateway support (#2)
    * Add support for push gateway
    
    * Add http port as a parameter
    
    * Add location metric label
    
    * Location metric label as string

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python
 
-
+import os
 import Adafruit_DHT
 import time
-from prometheus_client import start_http_server, Gauge
-
-sensor = Adafruit_DHT.AM2302
-pin = 4
+from prometheus_client import start_http_server, Gauge, CollectorRegistry, push_to_gateway
 
 
-temperature_gauge = Gauge('temperature', 'Temperature')
-humidity_gauge = Gauge('humidity', 'Humidity')
+def get_sensor(sensor_string):
+    if sensor_string == "DHT11":
+        return Adafruit_DHT.DHT11
+    elif sensor_string == "DHT22":
+        return Adafruit_DHT.DHT22
+    elif sensor_string == "AM2302":
+        return Adafruit_DHT.AM2302
+    else:
+        raise Exception("Invalid sensor type (%s)" % sensor_string)
 
 
+pushGatewayServer = os.environ.get('PUSH_GATEWAY_SERVER')
+sensor = get_sensor(os.environ.get('SENSOR_TYPE'))
+pin = int(os.environ.get('SENSOR_PIN', '4'))
+httpPort = int(os.environ.get('HTTP_PORT', '8000'))
+location = os.environ.get('LOCATION', '')
 
+registry = CollectorRegistry()
+temperature_gauge = Gauge('temperature', 'Temperature', ['location'], registry=registry)
+humidity_gauge = Gauge('humidity', 'Humidity', ['location'], registry=registry)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(8000)
-    # Generate some requests.
+    start_http_server(httpPort)
     while True:
         time.sleep(2)
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-        temperature_gauge.set(temperature)
-        humidity_gauge.set(humidity)
+
+        temperature_gauge.labels(location).set(temperature)
+        humidity_gauge.labels(location).set(humidity)
+
+        if pushGatewayServer:
+            push_to_gateway(pushGatewayServer, job='DTHPrometheus', registry=registry)
+
+
+
